cbc32c1cc1f40a6063eb322f4cc7fb33f2951986
Alina Pelaggi <161222529+AlinaPelaggi@users.noreply.github.com>
2024-03-31 22:17:02 +0200

    Update hw5_ex4.py

--- hw5_ex4.py
+++ hw5_ex4.py
@@ -1,55 +1,68 @@
 #Четверте завдання
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            return "The contact exists"
+        except ValueError:
+            return "Please enter the correct arguments"
+        except IndexError:
+            return "No such contacts"
+    return inner
+
+@input_error
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
 
-def input_error(func):
-    def wrapper(*args, **kwards):
-        try:
-            return func(*args, **kwards)
-        except (KeyError, ValueError, IndexError) as e:
-            error_message = {
-                KeyError: "Please enter user name",
-                ValueError: "Enter name and phone number please",
-                IndexError: "Invalis index. Please try again"
-            }
-            print(error_message.get(type(e), "Error accured. Please try again."))
-            return wrapper
-
-@input_error  
+@input_error
 def add_contact(args, contacts):
+    if not args or len(args) != 2:
+        raise ValueError
     name, phone = args
+    if name in contacts:
+        raise KeyError
     contacts[name] = phone
     return "Contact added."
 
-@input_error  
+@input_error
 def change_contact(args, contacts):
+    if not args or len(args) != 2:
+        raise ValueError
     name, phone = args
-    if contacts.get(name):
-        contacts[name] = phone
-        return "Contact changed."
-    else:
-        return "Conctact not exist"
+    if name not in contacts:
+        raise IndexError
+    contacts[name] = phone
+    return "Contact updated successfully"
 
-@input_error  
-def show_phone(args, contacts):
+@input_error
+def show_contact(args, contacts):
     name = args[0]
-    contacts_phone = contacts.get(name)
-    if contacts_phone:
-        return contacts_phone
+    if name in contacts:
+        return contacts[args[0]] 
     else:
-        return "Conctact not exist"
+        raise IndexError
 
+@input_error
 def show_all(contacts):
-    return contacts
-
+    if not contacts:
+        raise IndexError
+    all_contacts = []
+    for name in contacts:
+        all_contacts.append(f'{name}: {contacts[name]}')
+        
+    return '\n'.join(all_contacts)
+    
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
+
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
+
         if command in ["close", "exit"]:
             print("Good bye!")
             break
@@ -57,14 +70,15 @@ def main():
             print("How can I help you?")
         elif command == "add":
             print(add_contact(args, contacts))
-        elif command =='change':
-            print(change_contact(args,contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
         elif command == "phone":
-            print(show_phone(args,contacts))    
+            print(show_contact(args, contacts))
         elif command == "all":
-            print(show_all(contacts))    
+            print(show_all(contacts))
         else:
-            print("Invalid command.")
+            print("Invalid command")
+
 
 if __name__ == "__main__":
     main()
